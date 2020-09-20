5fb6fc7597aa2f2e71b08db19346dba70f33a877
Ahuoyiza <sophieoyiza@gmail.com>
2020-09-20 22:41:36 +0100

    menu-items component navigation updated using HOC

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/HomePage.component";
 
 import "./App.css";
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>Hats page</h1>
+    </div>
+  );
+};
 
 class App extends Component {
   render() {
     return (
       <div>
-        <HomePage />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/hats" component={HatsPage} />
+        </Switch>
       </div>
     );
   }
